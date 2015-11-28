11760d31a53d56f4a567deddd774d127fe6ef487
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-11-28 13:09:57 -0500

    kind of working. need to figure out how to append as li

--- js/spotify.js
+++ js/spotify.js
@@ -1,30 +1,59 @@
 // API Docs at:
 // https://developer.spotify.com/technologies/web-api/search/
 
+$(document).ready( function() {
+  $(".submit").click(function(evt){
+    evt.preventDefault();
+    var keyword = $("input[id='search-keyword']").val();
+    var option = $('#search-type').val();
+    console.log(option);//just to test if click is working - should apepear as 'artist' or 'track' in console
+    if (option === "artist") {
+      searchByArtist(keyword);
+    } else {
+      searchByTrack(keyword)
+    }
+  })
+})
 
 function searchByArtist(keyword) {
   var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
+  ajax(url)
 }
-
 
 function searchByTrack(keyword) {
   var url = 'http://ws.spotify.com/search/1/track.json?q='+keyword;
+  ajax(url)
 }
 
-// will need to have if else statement for if artist is selected from dropdown or if song is selected from drop down
-$(document).ready(function(){
-  $("h1").on("click", function(){
-    var url = "https://api.wunderground.com/api/f28a93cae85945b6/geolookup/conditions/q/va/midlothian.json"
-    $.ajax({
-      url: url,
-      type: "get",
-      dataType: "json"
-    }).done(function(response){
-      console.log(response.current_observation.temp_f)
-    }).fail(function(){
-      console.log("Ajax request fails!")
-    }).always(function(){
-      console.log("This always happens regardless of successful ajax request or not.")
-    })
+var listArtist = function (response) {
+  for (var i=0;i<response.artists.length;i++) {
+    $('body').append(response.artists[i].name)
+  }
+}
+
+var listTrack = function (response) {
+  for (var i=0;i<response.tracks.length;i++) {
+    $('body').append(response.tracks[i].name)
+  }
+}
+
+//ajax function - appending result below search bar
+var ajax = function (url){
+  var option = $('#search-type').val();
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "json"
+  }).done ( function(response){
+    console.log(response.option);
+    if (option === "artist") {
+      listArtist(response);
+    } else {
+      listTrack(response);
+    }
+  }).fail ( function (){
+    console.log("Failure");
+  }).always( function(){
+    console.log("This will happen regardless");
   })
-});
+}
