ce80c813df9329778e74af440b4fc445b9574e7c
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-11-28 12:24:40 -0500

    initial commit, set up to add api

--- js/spotify.js
+++ js/spotify.js
@@ -1,4 +1,4 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/technologies/web-api/search/
 
 
@@ -10,3 +10,21 @@ function searchByArtist(keyword) {
 function searchByTrack(keyword) {
   var url = 'http://ws.spotify.com/search/1/track.json?q='+keyword;
 }
+
+// will need to have if else statement for if artist is selected from dropdown or if song is selected from drop down
+$(document).ready(function(){
+  $("h1").on("click", function(){
+    var url = "https://api.wunderground.com/api/f28a93cae85945b6/geolookup/conditions/q/va/midlothian.json"
+    $.ajax({
+      url: url,
+      type: "get",
+      dataType: "json"
+    }).done(function(response){
+      console.log(response.current_observation.temp_f)
+    }).fail(function(){
+      console.log("Ajax request fails!")
+    }).always(function(){
+      console.log("This always happens regardless of successful ajax request or not.")
+    })
+  })
+});
--- readme.md
+++ readme.md
@@ -4,10 +4,10 @@ Use Ajax to load data from the Spotify API! A user should be able to:
 
 ## Basic Requirements:
 
- - Enter a keyword to seach for.
+ - Enter a keyword to search for.
  - Select "artist" or "track" as the search type.
  - Have the names of all search results print as a list.
- 
+
 ## Keep going:
 
  - Add a "Showing X-X of X results found" message.
