acbbb229bae77f6b8d3192aeefb780cbb8f97d76
Daniel Alexander <daniel.elliot.alexander@gmail.com>
2015-11-29 21:44:35 -0500

    initial commit

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,31 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/technologies/web-api/search/
-
-
-function searchByArtist(keyword) {
+$(document).ready(function(){
+  $("submit").on("click", function searchByArtist(keyword) {
   var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
-}
+    $.ajax({
+      url: url,
+      type: "get",
+      dataType: "json"
+    }).done(function(){
+      // populate list of artists
+    }).fail(function(){
+      console.log("Ajax request fails!")
+    })
+  })
+})
 
-
-function searchByTrack(keyword) {
+$(document).ready(function(){
+  $("submit").on("click", function searchByTrack(keyword) {
   var url = 'http://ws.spotify.com/search/1/track.json?q='+keyword;
-}
+    $.ajax({
+      url: url,
+      type: "get",
+      dataType: "json"
+    }).done(function(){
+      // populate list of tracks
+    }).fail(function(){
+      console.log("Ajax request fails!")
+    })
+  })
+})
