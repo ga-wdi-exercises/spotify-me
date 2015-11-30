c168b889ef2d6bd58e6374f1e27c0447a5d43a6b
Daniel Alexander <daniel.elliot.alexander@gmail.com>
2015-11-30 14:36:23 -0500

    working non dry solution for artists

--- js/spotify.js
+++ js/spotify.js
@@ -1,19 +1,29 @@
 // API Docs at:
 // https://developer.spotify.com/technologies/web-api/search/
 $(document).ready(function(){
-  $("submit").on("click", function searchByArtist(keyword) {
+  $(":submit").on("click", function searchByArtist(event) {
+    event.preventDefault();
+    var keyword = $("#search-keyword").val()
+    var keywordType = $("#search-type").val()
+    console.log(keyword)
+    console.log(keywordType)
   var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
     $.ajax({
       url: url,
       type: "get",
       dataType: "json"
-    }).done(function(){
-      // populate list of artists
+    }).done(function(response){
+      if (keywordType == "artist"){
+        for (i = 0; i <= response.artists.length; i++){
+        $("#results").append("<li>"+response.artists[i].name+"</li>")
+      }
+    }
     }).fail(function(){
       console.log("Ajax request fails!")
     })
   })
 })
+
 
 $(document).ready(function(){
   $("submit").on("click", function searchByTrack(keyword) {
