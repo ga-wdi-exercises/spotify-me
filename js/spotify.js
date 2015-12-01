e19309accc8f1980335e660e9b4e558de74af30a
Daniel Alexander <daniel.elliot.alexander@gmail.com>
2015-11-30 21:03:53 -0500

    dryer code by a little and added search tracks

--- js/spotify.js
+++ js/spotify.js
@@ -1,41 +1,29 @@
 // API Docs at:
 // https://developer.spotify.com/technologies/web-api/search/
 $(document).ready(function(){
-  $(":submit").on("click", function searchByArtist(event) {
+  $(":submit").on("click", function searchBy(event) {
     event.preventDefault();
     var keyword = $("#search-keyword").val()
-    var keywordType = $("#search-type").val()
-    console.log(keyword)
-    console.log(keywordType)
-  var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
+    var type = $("#search-type").val()
+    var url = 'http://ws.spotify.com/search/1/' + type + '.json?q=' + keyword;
     $.ajax({
       url: url,
       type: "get",
       dataType: "json"
     }).done(function(response){
-      if (keywordType == "artist"){
+      if (type == "artist"){
         for (i = 0; i <= response.artists.length; i++){
-        $("#results").append("<li>"+response.artists[i].name+"</li>")
+          $("#results").append("<li>"+response.artists[i].name+"</li>")
+        }
       }
-    }
-    }).fail(function(){
-      console.log("Ajax request fails!")
+      if (type == "track"){
+        console.log(response.tracks)
+        for (i = 0; i <= response.tracks.length; i++){
+          $("#results").append("<li>" +response.tracks[i].name+"</li>")
+        }
+      }
+      }).fail(function(){
+        console.log("Ajax request fails!")
+      })
     })
   })
-})
-
-
-$(document).ready(function(){
-  $("submit").on("click", function searchByTrack(keyword) {
-  var url = 'http://ws.spotify.com/search/1/track.json?q='+keyword;
-    $.ajax({
-      url: url,
-      type: "get",
-      dataType: "json"
-    }).done(function(){
-      // populate list of tracks
-    }).fail(function(){
-      console.log("Ajax request fails!")
-    })
-  })
-})
