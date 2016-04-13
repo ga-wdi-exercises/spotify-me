97e18dfd53830096a3ab17821d58dd652ffecd04
Salomone Baquis <salomone.l.baquis@gmail.com>
2016-04-12 23:16:27 -0400

    basic working tool

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,56 @@
 // API Docs at: 
 // https://developer.spotify.com/web-api/search-item/
 
+// form on submit
+// get the user input
+// get the search results from api
+	// loop through each artist or track
+	// create an <li></li>
+	// set the html of the li to the results
+	// append the results to the #results list
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
+var $form = $("#search");
+var $dropdown = $("search-type");
+var $searchField = $("#search-keyword");
+var $resultsList = $("#results");
+var artist = $("#artist")
+var track = $("#track")
 
+$form.on("submit", function(event) {
+	event.preventDefault();
+	var userInput = $searchField.val();
+	console.log(userInput)
+	
+	if ( document.getElementById("artist").selected ) {
+		$.ajax({
+			url: "https://api.spotify.com/v1/search?q=" + userInput + "&type=artist",
+			success: function(response) {
+				$("li").remove()
+				var artists = response.artists.items
+				console.log(artists[0].name)
+				for (var i = 0; i < artists.length; i++) {
+					var artistNames = artists[i].name;
+					var $newLi = $("<li></li>")
+					var artistNameList = $newLi.html(artistNames)
+					$resultsList.append(artistNameList)
+				}
+			}
+		})
+	}
+	else if ( document.getElementById("track").selected ) {
+		$.ajax({
+			url: "https://api.spotify.com/v1/search?q=" + userInput + "&type=track",
+			success: function(response) {
+				$("li").remove()
+				var tracks = response.tracks.items				
+				for (var i = 0; i < tracks.length; i++) {
+					var trackNames = tracks[i].name;
+					var $newLi = $("<li></li>")
+					var trackNameList = $newLi.html(trackNames)
+					$resultsList.append(trackNameList)
+				}
+			}
+		})	
+	}
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+})
