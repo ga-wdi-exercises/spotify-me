9c69c3be79a354ffe3da838f280c3f0e742d4c75
Matthew White <matthew-white@users.noreply.github.com>
2016-04-13 14:20:57 -0400

    White space changes

--- js/spotify.js
+++ js/spotify.js
@@ -1,11 +1,9 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
-
 
 function searchByArtist(keyword) {
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
 }
-
 
 function searchByTrack(keyword) {
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
