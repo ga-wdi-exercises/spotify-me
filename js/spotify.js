0f0baab9f20604c249da1e8057a8605ed70a0095
Matthew White <matthew-white@users.noreply.github.com>
2016-04-14 20:52:45 -0400

    First version
    - Add basic search functionality.
    - After reviewing the API, "search for" seemed more accurate than
      "search by".
    - No error handling yet.
    - No pagination.

--- js/spotify.js
+++ js/spotify.js
@@ -1,10 +1,105 @@
-// API Docs at:
-// https://developer.spotify.com/web-api/search-item/
+$(function() {
+  getApp().initPage().listen();
+});
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
+function getApp() {
+  var searchTypes = defineSearchTypes();
+  var form = defineForm();
+  var results = defineResults();
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+  return {
+    initPage: function() {
+      form.searchType.init();
+      return this;
+    },
+    listen: function() {
+      form.element.submit(function(event) {
+        event.preventDefault();
+        search(searchTypes[form.searchType.val()], form.keyword.val());
+      });
+      return this;
+    }
+  };
+
+  function defineSearchTypes() {
+    return ['artist', 'track'].map(function(name) {
+      return {
+        name: name,
+        plural: name + 's'
+      };
+    });
+  }
+
+  function defineForm() {
+    var element = $('#search');
+    var searchType = defineSearchType();
+    var keyword = defineKeyword();
+    aliasVal([searchType, keyword]);
+    return {
+      element: element,
+      searchType: searchType,
+      keyword: keyword
+    };
+
+    function defineSearchType() {
+      var element = $('#search-type');
+      return {
+        element: element,
+        init: function() {
+          searchTypes.forEach(function(type, index) {
+            var $option = $('<option>')
+              .val(index)
+              .text('Search for ' + type.name);
+            element.append($option);
+          });
+        }
+      };
+    }
+
+    function defineKeyword() {
+      return { element: $('#search-keyword') };
+    }
+
+    // For each form field, alias .element.val as .val for easy access.
+    function aliasVal(fields) {
+      fields.forEach(function(field) {
+        field.val = function() {
+          if (arguments.length)
+            return field.element.val(arguments[0]);
+          else
+            return field.element.val();
+        };
+      });
+    }
+  }
+
+  function defineResults() {
+    var element = $('#results');
+    return {
+      element: element,
+      clear: function() {
+        element.empty();
+      },
+      add: function(results) {
+        results.items.forEach(function(item) {
+          element.append($('<li>').text(item.name));
+        });
+      }
+    };
+  }
+
+  function search(type, keyword) {
+    results.clear();
+    var url = 'https://api.spotify.com/v1/search?' + $.param({
+      type: type.name,
+      q: keyword
+    });
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      success: function(data) {
+        results.add(data[type.plural]);
+      }
+    });
+  }
 }
