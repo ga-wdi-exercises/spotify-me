b7cfe4819925a63f73d011c8eaa5d81fcc926eb2
Megan Coyle <meg@mcoyle.com>
2016-04-18 13:09:15 -0400

    Clear appends on select

--- js/spotify.js
+++ js/spotify.js
@@ -5,6 +5,7 @@ var $form = $("#search");
 var $dropdown = $("#search-type");
 var $results = $("#results");
 var $numResults = $("#number-results");
+var numItems = 0;
 
 // clear input on click
 $input.click(function() {
@@ -20,29 +21,23 @@ $form.on("submit", function(e){
   e.preventDefault()
   // clear previous results
   $results.html("");
-  $numResults.html("");
   if ($dropdown.val() === "artist"){
   searchByArtist($input.val());
   } else if ($dropdown.val() === "track"){
   searchByTrack($input.val());
   }
+  $numResults.html("<em>There are" + " " + numItems + " " + "items available.</em>");
 })
 
-// select Search by Track
-  // submit
-  // append new li for ul id="results" for search results
-
 function searchByArtist(keyword) {
-  console.log(keyword);
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
   $.get(url, function(response){
-    console.log(response)
       for (i = 0; i < response.artists.items.length; i++) {
         var $resultItem = $("<li>" + response.artists.items[i].name + "</li>")
         $results.append($resultItem);
       }
-      var numItems = response.artists.items.length;
-      $numResults.html("<em>There are" + " " + numItems + " " + "items available.</em>")
+      var artistItems = response.artists.items.length;
+      numItems = artistItems;
   })
 }
 
@@ -50,12 +45,11 @@ function searchByArtist(keyword) {
 function searchByTrack(keyword) {
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
   $.get(url, function(response){
-    console.log(response)
       for (i = 0; i < response.tracks.items.length; i++) {
         var $resultItem = $("<li>" + response.tracks.items[i].artists[0].name + ": " + "<strong><em>" + response.tracks.items[i].name + "</em></strong>" + " from " + response.tracks.items[i].album.name + "</li>")
         $results.append($resultItem)
       }
-      var numItems = response.tracks.items.length;
-      $numResults.html("<em>There are" + " " + numItems + " " + "items available.</em>")
+      var trackItems = response.tracks.items.length;
+      numItems = trackItems;
   })
 }
