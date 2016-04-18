760869facef7c416240bef335b16e8dcd25a4e81
chekosa <cherrilyn.soliman@gmail.com>
2016-04-18 19:40:06 -0400

    test commit

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,48 @@
-// API Docs at: 
-// https://developer.spotify.com/web-api/search-item/
+// // API Docs at:
+// // https://developer.spotify.com/web-api/search-item/
+//
+//
+// function searchByArtist(keyword) {
+//   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+// }
+//
+//
+// function searchByTrack(keyword) {
+//   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+// }
 
+function searchSpotify(event) {
+  event.preventDefault();
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+  var $term = $("#search-keyword").val();
+  var $searchType = $("#search-type").val();
+
+  var url = 'https://api.spotify.com/v1/search?q=' + $term + '&type=' + $searchType
+  console.log($term, $searchType, url);
+
+  $.ajax({
+    url: url,
+    method: "GET"
+  }).done(function(data) {
+    var resultsProperty = $searchType + "s";
+    var results = data[resultsProperty]["items"]
+    displayResults(results);
+    console.log(results);
+  });
+}
+
+function displayResults(results) {
+  var $container = $("#results");
+  console.log(results);
+  $container.empty();
+  results.forEach(function(result) {
+    // $container.append("<li><a href='" + result.external_urls.spotify + "' target='_blank'>" + result.name + "</a></li>");
+    $container.append("<li> " + result.name + " </li>");
+  })
 }
 
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+
+$(function() {
+  $('form#search input[type=submit]').on("click", searchSpotify);
+});
