a668bec2e8adb19cf2301d9ccd8b8f688ebc0a13
chekosa <cherrilyn.soliman@gmail.com>
2016-04-20 16:53:38 -0400

    final commit

--- js/spotify.js
+++ js/spotify.js
@@ -10,13 +10,18 @@
 // function searchByTrack(keyword) {
 //   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
 // }
+//***save time instead of doing two runs
 
+//create a function to connect and make request to the Spotify API
 function searchSpotify(event) {
   event.preventDefault();
-
+//capture what keyword the user is searching for
+//capture the dropdown value selcted
   var $term = $("#search-keyword").val();
   var $searchType = $("#search-type").val();
-
+//simplify the spotify endpoint urls
+//use ajax to ping the API
+//pass the results to .done method add "s" to the search-type
   var url = 'https://api.spotify.com/v1/search?q=' + $term + '&type=' + $searchType
   console.log($term, $searchType, url);
 
@@ -24,25 +29,26 @@ function searchSpotify(event) {
     url: url,
     method: "GET"
   }).done(function(data) {
-    var resultsProperty = $searchType + "s";
-    var results = data[resultsProperty]["items"]
+    var resultsDisplay = $searchType + "s";
+    var results = data[resultsDisplay]["items"]
     displayResults(results);
     console.log(results);
   });
 }
-
+//create function to display results and insert to the index.html <ul id="results">
+//loop each result and add another line using <li> tag to display each result in separate line
 function displayResults(results) {
   var $container = $("#results");
   console.log(results);
   $container.empty();
   results.forEach(function(result) {
-    // $container.append("<li><a href='" + result.external_urls.spotify + "' target='_blank'>" + result.name + "</a></li>");
     $container.append("<li> " + result.name + " </li>");
   })
 }
 
 
-
+//show the result when user click the submit button
+//use searchSpotify as callback function for the event handler .on(click)
 $(function() {
   $('form#search input[type=submit]').on("click", searchSpotify);
 });
