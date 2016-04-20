50a8b4e72b52081d93f5df76bfa94ed0d1e5e3fe
Natalie Beach <nataliehbeach@gmail.com>
2016-04-20 19:15:42 -0400

    homework answer

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,34 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
 
+var $form = $("#search")
+var $input = $("#search-keyword")
 
 function searchByArtist(keyword) {
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+    $.ajax({
+      url: url,
+      success: function(response) {
+        console.log(response)
+      }
+    })
 }
-
 
 function searchByTrack(keyword) {
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+    $.ajax({
+      url: url,
+      success: function(response) {
+        console.log(response)
+      }
 }
+
+//on form submit
+$form.on("submit", function(event) {
+  event.preventDefault()
+  var keyword = $input.val()
+  //if option value === artist??
+  searchByArtist(keyword);
+  //if option value === track
+  searchByTrack(keyword);
+})
