b150f13f83101bf8c54a28e9d69369d3a3df026c
Julia Rose <jmrose@pbs.org>
2016-04-20 16:56:45 -0400

    more refactoring, adding comments

--- js/spotify.js
+++ js/spotify.js
@@ -9,28 +9,31 @@ const $searchKeyword = $('#search-keyword');
 const $selectedOption = $(':selected');
 const $search = $('#search');
 
-
+// Take user search term and determine whether to search it as track or artist
 function getKeyword(e) {
   e.preventDefault();
+
+  // clear old results, if any
   $results.empty();
 
   var keyword = $searchKeyword.val();
 
   if ($selectedOption.val() === 'artist') {
-    searchSpotify(keyword, 'artist');
+    composeQuery(keyword, 'artist');
   } else {
-    searchSpotify(keyword, 'track');
+    composeQuery(keyword, 'track');
   }
 };
 
-function searchSpotify(keyword, searchType) {
+// Using search term and search type, compose the Spotify endpoint URL
+function composeQuery(keyword, searchType) {
   var url = 'https://api.spotify.com/v1/search?q=' + keyword + '&type=' + searchType;
 
-  makeAPICall(url);
+  searchSpotify(url);
 };
 
-function makeAPICall(url) {
-  console.log("calling...");
+// Make a call to Spotify API with the specified URL
+function searchSpotify(url) {
   $.ajax({
     url: url,
     method: 'GET',
@@ -39,6 +42,7 @@ function makeAPICall(url) {
   });
 }
 
+// Iterate through list of results, adding them to the DOM
 function handleSuccess(response) {
   var data = response.artists ? response.artists : response.tracks;
 
@@ -49,20 +53,32 @@ function handleSuccess(response) {
     $results.append(listItem);
   });
 
-  if (data.total > 20 ) {
-    addResultCount(data);
-  } 
+  // Display number of results
+  addResultCount(data);
 };
 
-
+// 
 function addResultCount(data) {
   var first = data.offset + 1;
   var last = data.offset + 20;
   var total = data.total;
 
-  var nowShowing = $('<p></p>').text('Showing ' + first + ' - ' + last + ' of ' + total);
+  if (total === 0) {
+    var nowShowing = $('<p></p>').text('No results found.');
+  } 
+  else if (total > 20) {
+    // e.g. 'Showing 1 - 20 of 500'
+    var nowShowing = $('<p></p>').text('Showing ' + first + ' - ' + last + ' of ' + total);
+  } 
+  else {
+    // e.g. 'Showing 1 - 5 of 5'
+    var nowShowing = $('<p></p>').text('Showing ' + first + ' - ' + total + ' of ' + total);
+  }
+
+  // Add page count to the DOM
   $results.append(nowShowing);
 
+  // Determine which pagination links are needed
   if (data.previous) {
     addPagination(data, 'previous');
   }
@@ -72,6 +88,7 @@ function addResultCount(data) {
   } 
 }
 
+// Create and append pagination links
 function addPagination(data, direction) {
   var pageLink = $('<a href="#" class=' + direction + '></a>').text(direction);
   $results.append(pageLink);
@@ -80,14 +97,16 @@ function addPagination(data, direction) {
   $('.previous').on('click', {dataList: data, direction: 'previous'}, onPageClick);
 }
 
+// Handle clicks to pagination links
 function onPageClick(event) {
+  // clear current page of results
   event.stopImmediatePropagation();
-
   $results.empty();
-  var url = event.data.dataList[event.data.direction];
-  console.log(url);
 
-  makeAPICall(url);
+  // Compose endpoint url -- this becomes url = 'data.next' or 'data.previous'
+  var url = event.data.dataList[event.data.direction];
+
+  searchSpotify(url);
 }
 
 // Listen for a submit in the search field
