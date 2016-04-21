41186382b704086a521fbcf9aee7e134ce264a0b
Libbygi <isenstein.libby@gmail.com>
2016-04-20 21:27:11 -0400

    final commit

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,52 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
 
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+var $form = $('#search');
+var $input = $('#search-keyword');
+var $results = $('#results');
+var $pagination = $('#pagination');
+var $next = $('#next');
+
+$form.on("submit", function(e){
+  e.preventDefault()
+  var keyword = $input.val();
+  var $searchType = $('#search-type option:selected').val();
+  var $typePlural = $searchType + 's';
+
+  var url = 'https://api.spotify.com/v1/search?q=' + keyword + '&type=' + $searchType;
+    $.get(url, function(response){
+        printList(response[$typePlural].items);
+        paginate(response);
+  })
+
+})
+
+function printList(itemList){
+  var resultsShowing = itemList.length;
+
+  $results.empty();
+  itemList.forEach(function(item){
+    var $li = $("<li id='" + item.id + "'>" + item.name + "</li>");
+    $results.append($li);
+    $pagination.html('Showing ' + resultsShowing + ' results.');
+  })
 }
 
+//Figure out how to replace URL so that
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+// function paginate(object){
+//   var $searchType = $('#search-type option:selected').val();
+//   var $typePlural = $searchType + 's';
+//   var nextLink = object[$typePlural].next;
+//   $next.html('<a href = "' + nextLink + '"> Next </a>');
+//
+// }
+
+// function searchByTrack(keyword) {
+//   var url = 'https://api.spotify.com/v1/search?q=' + keyword + '&type=track';
+// }
+
+// function searchByArtist(keyword) {
+//   var url = 'https://api.spotify.com/v1/search?q=' + keyword + '&type=artist';
+// }
