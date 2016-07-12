f4ff29cc008215e7ab1841d64b6cf2936ed4860b
Alfredo Amengual <amengual.alfredo@gmail.com>
2016-07-12 18:33:08 -0500

    hw done

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,48 @@
-// API Docs at: 
-// https://developer.spotify.com/web-api/search-item/
+// API Docs at:
+$(document).ready(function() {
+  $('#search').on('submit', function(event){
+      event.preventDefault();
+      var keyword = $('#search-keyword').val();
+      searchByArtist(keyword);
+      searchByTrack(keyword);
+  });
 
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+});
+
+
+
+
+  function searchByArtist(keyword) {
+    var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+    $.ajax({
+      method: "GET",
+      url: url,
+      dataType: "json"
+    })
+    .done(function(data){
+      var artistsList = data.artists.items;
+      artistsList.forEach(function(artist){
+        console.log(data.artists.length);
+        $('#results').append("<li>" + artist.name + "</li>");
+      })
+    });
 }
 
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+
+  function searchByTrack(keyword) {
+    var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+    $.ajax({
+      method: "GET",
+      url: url,
+      dataType: "json"
+    })
+    .done(function(data){
+      var tracksList = data.tracks.items;
+      tracksList.forEach(function(track){
+        $('#results').append("<li>" + track.name + "</li>");
+      })
+    });
+  }
+
