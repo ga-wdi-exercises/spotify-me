1f0bde2408baed841422206f9b2d12890b9ef028
camille morell <cemorell>
2016-07-13 09:05:12 -0500

    trying to loop through array but cannot tell if it is jason or jasonp

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,46 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+$(document).ready(function(){
+
+  $("#search").on("submit", function(evt) {
+    evt.preventDefault();
+    var keyword = $("#search-keyword").val();
+    console.log(keyword)
+    var selectedValue = $('#search-type').val();
+
+    if(selectedValue  === 'artist') {
+      searchByArtist(keyword);
+    } else  {
+      searchByTrack(keyword);
+    }
+  });
+});
 
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
+
+    function searchByArtist(keyword) {
+      var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+
+      $.ajax({
+        Method: "Get",
+        url: url,
+        dataType: "jsonp",
+        jsonpCallback: "showImage"
+      });
+
+         // .done(function(data, textStatus, jqXHR) {
+         //   // $("#results").html()
+         // }
+
+       };
 
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+
+       function searchByTrack(keyword) {
+        var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+
+
+
+
+      }
+
