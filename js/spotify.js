9124983c72253718b53dafbbea91e8787d2a1191
Zach Gaines <zrgaines@mtu.edu>
2016-07-12 22:21:43 -0500

    ajax set up

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,51 @@
 // API Docs at: 
 // https://developer.spotify.com/web-api/search-item/
 
+$(document).ready(function() {
+  $('#search').submit(function(e) {
+    if($('#search-type').val() == 'artist') {
+      searchByArtist($('#search-keyword'));
+    }
+    else {searchByTrack($('#search-keyword'));}
+  })
+})
+
 
 function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+  keyword = keyword.val();
+
+  var url = 'https://api.spotify.com/v1/search';
+  debugger
+  $.ajax({
+    url: url,
+    method: "GET",
+    dataType: 'json',
+    data: {
+      q: keyword,
+      type: artist
+    }
+  })
+  .done(function(artistResponse){
+    debugger
+  })
 }
 
 
 function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+  keyword = keyword.val();
+
+  var url = 'https://api.spotify.com/v1/search';
+
+  $.ajax({
+    url: url,
+    method: "GET",
+    dataType: 'json',
+    data: {
+      q: keyword,
+      type: track
+    }
+  })
+  .done(function(songResponse){
+    debugger
+  })
 }
