a1080414d93a1ec7b838a2e0c401cc0ac7f5eed0
Robert Reilley <rpreilley@gmail.com>
2016-07-12 20:51:52 -0500

    homework

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,45 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+$(document).ready(function() {
+  $('#search').submit(function(event) {
+    event.preventDefault();
+    var keyword = $('#search-keyword').val();
+    var selectedValue = $('#search-type').val();
+      if(selectedValue  === 'artist') {
+        searchByArtist(keyword);
+      } else  {
+        searchByTrack(keyword);
+      }
+  })
 
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
+  function searchByArtist(keyword) {
+    var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+    $.ajax({
+      method: "GET",
+      url: url,
+      dataType: "json"
+    })
+    .done(function(response) {
+      var artistObject = response.artists.items
+      artistObject.forEach(function(data) {
+        $('#results').append("<li>" + data.name + "</li>");
+      })
+    })
+  }
 
-
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+  function searchByTrack(keyword) {
+    var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+    $.ajax({
+      method: "GET",
+      url: url,
+      dataType: "json"
+    })
+    .done(function(response) {
+      var trackObject = response.tracks.items;
+      trackObject.forEach(function(track) {
+        $('#results').append("<li>" + track.name + "</li>");
+      })
+    })
+  }
+});
