d0a051f112acee0993bb0205a2fe95dc4a4f7134
camille morell <cemorell>
2016-07-14 10:04:53 -0500

    DONE (finally)

--- js/spotify.js
+++ js/spotify.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   $("#search").on("submit", function(evt) {
     evt.preventDefault();
     var keyword = $("#search-keyword").val();
-    console.log(keyword)
+    // console.log(keyword)
     var selectedValue = $('#search-type').val();
 
     if(selectedValue  === 'artist') {
@@ -19,34 +19,41 @@ $(document).ready(function(){
 
 
     function searchByArtist(keyword) {
-      var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-
+      var keyword = $("#search-keyword").val();
+      var url = "https://api.spotify.com/v1/search?q="+keyword+"&type=artist";
       $.ajax({
-        Method: "Get",
-        url: url,
-        dataType: "jsonp",
-        jsonpCallback: "showArtist"
-      });
-
-         // .done(function(data, textStatus, jqXHR) {
-         //   // $("#results").html()
-         // }
-
+        Method: "GET",
+        url: url
+      })
+      .done(function(response){
+        $('#results').html(" ")
+        response.artists.items.forEach(function(object){
+          console.log(object)
+          var artistName = object.name;
+          $('#results').append(`<li>${artistName}</li>`)
+        });
+       });
      };
 
 
-  function showArtist(jason){
-    var artistName = jason.artists.name;
-    $('#results').html(`<li>${artistName}</li>`)
-    }
 
 
-
-       function searchByTrack(keyword) {
+    function searchByTrack(keyword) {
         var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+      $.ajax({
+        Method: "GET",
+        url: url,
+        dataType: "json",
+        jsonpCallback: "showArtist"
+      })
+      .done(function(response){
+        $('#results').html(" ")
+        response.artists.items.forEach(function(object){
+          console.log(object)
+          var songName = object.name;
+          $('#results').append(`<li>${songName}</li>`)
+        });
+       });
 
-
-
-
-      }
+      };
 
