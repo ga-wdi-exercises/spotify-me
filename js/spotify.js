6c9448bb2240dea8b1850cdfca4c093481856bbc
Bryony McLaughlin <bryonymc@gmail.com>
2016-10-05 17:41:48 -0500

    added fdn of html and js

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,22 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+$(document).ready(function(){
+  var user_input = $('input #search-keyword').val();
+  $(user_input).on("submit", 'artist', searchSpotify)
 
-
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
-
-
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+});
+function searchSpotify() {
+  if(user_input == "Search by artist"){
+    function searchByArtist(keyword) {
+      event.preventDefault();
+      alert('it works!');
+      var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+      }
+    } else if(user_input == "search by track"){
+      function searchByTrack(keyword) {
+        event.preventDefault();
+        alert('it works!')
+        var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+      }
+    }
+  }
