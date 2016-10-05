406b9d979246b70be363bc070ad22bec474ecc52
jeauxy <jeauxcal@gmail.com>
2016-10-05 18:34:34 -0500

    Compelted Spotify HW & forgot to commit throughout

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,47 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+$(document).ready(function() {
+
+});
+    $('#search').submit(function(e) {
+    e.preventDefault();
+
+    var keyword = $('#search-keyword').val();
+    var selectValue = $('#search-type').val();
+      if(selectValue  === 'artist') {
+        searchArtist(keyword);
+      } else  {
+        searchTrack(keyword);
+      }
+  })
 
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
+  function searchArtist(keyword) {
+    var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+    $.ajax({
+      url: url,
+      // method: "Get",
+      // dataType: "json"
+    })
+      .done(function(response) {
+      var artistObject = response.artists.items
+      artistObject.forEach(function(data) {
+        $('#results').append("<li>" + data.name + "</li>");
+      })
+    })
+  };
 
-
-function searchByTrack(keyword) {
+  function searchTrack(keyword) {
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+    $.ajax({
+      url: url,
+      // method: "Get",
+      // dataType: "json"
+    })
+      .done(function(response) {
+      var artistObject = response.tracks.items;
+      artistObject.forEach(function(data) {
+        $('#results').append("<li>" + data.name + "</li>");
+      })
+    })
+  };
