656bf435f8d0bed38e85af172c1c0d70615486c3
Nathan Jensby <njensby@gmail.com>
2016-10-05 22:35:45 -0500

    added event listener and api call for searchByArtist function

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,36 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+$(document).ready(function (){
+  $('#search').on('submit', checkInput)
+  })
 
+function checkInput(e) {
+    e.preventDefault();
+    var $input = $('#search-keyword').val();
+    var $category = $('#search-type').val();
+    if ($category === "artist") {
+        searchByArtist($input);
+    } else {
+      searchByTrack($input);
+    }
+}
 
 function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+  var options = { url: 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist'
+  }
+  console.log('searched by artist', keyword);
+  var results = $.ajax(options);
+  results.done(function(object){
+    console.log(object);
+
+  });
+  results.fail(function(jqX, status, errorThrown) {
+    console.log(status, errorThrown);
+  });
 }
 
 
 function searchByTrack(keyword) {
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+  console.log('searched by track', keyword);
 }
