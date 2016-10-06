3d7267b2cec136438e038c85df79e67160182eb9
Kelvan Ince <incekelvan@gmail.com>
2016-10-06 08:55:18 -0500

    I wasn't quite able to finish this. I had a few issues trying to figure out what information to insert into my functions console logs.

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,48 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+$(document).ready(function () {
+
+  $('#search-type artist').on('click', function(e) {
+    var artists = $(this).text();
+
+    searchByArtist(artists);
+    e.preventDefault();
+  })
+  searchByArtist('#results');
+
+  function searchByArtist(keyword) {
+    var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+    //console.log();
+  }
+
+  $('#search-type track').on('click', function(e) {
+    var tracks = $(this).text();
+
+    searchByTrack(tracks);
+    e.preventDefault();
+  })
+  searchByTrack('#results')
 
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
+  function searchByTrack(keyword) {
+    var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+    // console.log();
+  }
+
+  var request = $.ajax(options);
+
+  request.done(function(output) {
+  })
+
+  equest.fail(function(jqXHR, status, errorThrown) {
+    console.log('Error', errorThrown);
+  });
+
+  request.always(function() {
+    console.log('Always');
+  });
 
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+searchByArtist();
+searchByTrack();
+});
