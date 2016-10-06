81a336f06f15f3f59f3638440b25a431f18507b1
Nathan Jensby <njensby@gmail.com>
2016-10-05 22:54:20 -0500

    finished minimum goals on hw

--- js/spotify.js
+++ js/spotify.js
@@ -18,12 +18,11 @@ function checkInput(e) {
 
 function searchByArtist(keyword) {
   var options = { url: 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist'
-  }
+  };
   console.log('searched by artist', keyword);
   var results = $.ajax(options);
   results.done(function(object){
     var list = object.artists.items;
-    console.log(list);
     for (var i = 0; i < list.length; i++) {
       var $listItem = $('<li />').appendTo('#results').html(list[i].name);
     }
@@ -35,6 +34,17 @@ function searchByArtist(keyword) {
 
 
 function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+  var options = { url: 'https://api.spotify.com/v1/search?q='+keyword+'&type=track'
+  };
   console.log('searched by track', keyword);
+  var results = $.ajax(options);
+  results.done(function(object){
+    var list = object.tracks.items;
+    for (var i = 0; i < list.length; i++) {
+      var $listItem = $('<li />').appendTo('#results').html(list[i].name);
+    }
+   });
+  results.fail(function(jqX, status, errorThrown) {
+    console.log(status, errorThrown);
+  });
 }
